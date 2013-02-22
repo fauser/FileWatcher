d51c80f737b1feca4d0c33da11cb9135f1be1d70
Daniel Svensson <fauser78@gmail.com>
2013-02-22 15:28:00 +0100

    First

---
+++ FileWatcher/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace FileWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Watcher w = new Watcher();
+
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
+                        break;
+                }
+            }
+        }
+
+
+    }
+}
